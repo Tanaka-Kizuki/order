7c43684788e1d03cfee487b218acdc6a65d6591c
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-09-23 23:43:11 +0900

    発注アクション追加

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Item;
+use Carbon\Carbon;
+use App\Order;
+use App\Data;
+
+
 
 class orderController extends Controller
 {
@@ -17,8 +22,34 @@ class orderController extends Controller
         for ($i=1;$i<=5;$i++) {
             $data[$i-1]=$datas[$i]['count'];
         }
-        echo '<pre>';
-        var_dump($datas);
-        echo '</pre>';
+        //発注日時の取得
+        $today = new Carbon();
+        $daily = $today->format('Y年m月d日');
+        $year = $today->year;
+        $month = $today->month;
+        $day = $today->day;
+
+        // 発注テーブル
+        $order = Order::create([
+            'today' => $daily,
+            'year' => $year,
+            'month' => $month,
+            'day' => $day,
+            'trader' => 'default' 
+        ]);
+
+        //発注明細テーブル
+        for ($i=1;$i<=5;$i++) {
+            $item = Item::where('id',3)->first();
+            $data = Data::create([
+                'order_id' => $order->id,
+                'name' => $datas[$i]['name'],
+                'count' => $datas[$i]['count'],
+                'total' => $item->prise * $datas[$i]['count'],
+            ]);
+        }
+
+        return redirect('/');
+
     }
 }
